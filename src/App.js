ecf2158dcc8507269b8c4c9598eae97833028e28
Muhammad Kurnia <muhammad.kurnia@larisin.id>
2020-08-12 14:23:32 +0700

    React & Ant Design

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import 'antd/dist/antd.css';
+import { Layout } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
