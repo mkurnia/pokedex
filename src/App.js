451b17af741895d25e2b12a65679b2f749d76867
Muhammad Kurnia <muhammad.kurnia@larisin.id>
2020-08-13 15:16:54 +0700

    Pokemon List & Filter By Type

--- src/App.js
+++ src/App.js
@@ -2,66 +2,81 @@ import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 import { Layout, Card, Col, Row, Select, Divider, Button } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 
 const { Header, Footer, Content } = Layout;
 const { Meta } = Card;
 const { Option } = Select;
 
-function usePrevious(value) {
-  const ref = useRef();
-  useEffect(() => {
-    ref.current = value;
-  });
-  return ref.current;
-}
-
 function App() {
+  const [loading, setLoading] = useState(true);
   const [pokemonList, setPokemonList] = useState([]);
+  const [pokemonTypeList, setPokemonTypeList] = useState([]);
   // const [pokemonDetail, setPokemonDetail] = useState(null);
   const [next, setNext] = useState(null);
-  const prevPokemonList = usePrevious(pokemonList);
 
 
-  const getData = () => {
-    let url = new URL('https://pokeapi.co/api/v2/pokemon')
-    if(next) {
-      url = new URL(next);
-    }
+  const getListPokemon = (url) => fetch(new URL(url)).then(response => response.json());
 
-    return fetch(url)
-      .then(response => response.json())
-      .then(responseData => {
-        console.log(responseData)
-        setNext(() => responseData.next)
-        setPokemonList(() => [...pokemonList, ...responseData.results]);
-      })
+  const getListPokemonByType = (url) => {
+    setLoading(true)
+    return fetch(new URL(url))
+    .then(response => response.json())
+    .then(responseData => {
+      setPokemonList(() => responseData.pokemon.map(data => data.pokemon));
+    })
+    .finally(() => {
+      setLoading(false)
+    })
   }
 
+  const getPokemonTypes = () => fetch(new URL('https://pokeapi.co/api/v2/type'))
+    .then(response => response.json())
+    .then(responseData => {
+      setPokemonTypeList(() => responseData.results);
+    })
+
+
+  const getListPokemenData = (url, { resetData = false } = {}) => {
+    setLoading(true)
+    return getListPokemon(url)
+    .then(data => {
+      if(resetData) {
+        setPokemonList(() => data.results)
+      } else {
+        setPokemonList(() => [...pokemonList, ...data.results])
+      }
+      setNext(() => data.next);
+    })
+    .finally(() => {
+      setLoading(false)
+    })
+  };
 
   useEffect(() => {
-    getData()
+    getListPokemenData('https://pokeapi.co/api/v2/pokemon');
+    getPokemonTypes();
   },[]);
-
-
 
   const constructImageUrl = (url) => {
     if (url) {
       let pokemonImgUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
-
       const split = url
         .split('/')
         .filter((val) => Number(val))
         .toString();
-
       return `${pokemonImgUrl}${split}.png`;
     }
-
     return '';
   };
 
-  const loadMore = () => getData();
-  const handleChangeType = (value) => {
-    console.log(value)
+  const loadMore = () => getListPokemenData(next);
+  const handleChangeType = async (url) => {
+    if(!url) {
+      getListPokemenData('https://pokeapi.co/api/v2/pokemon', { resetData: true })
+    } else {
+      getListPokemonByType(url)
+    }
   }
 
 
@@ -73,13 +88,20 @@ function App() {
         </Header>
         <Content>
           <div className="content-box site-card-wrapper">
-          <Select defaultValue="" style={{ width: 120 }} onChange={handleChangeType}>
-            <Option value="">All Type</Option>
-            <Option value="jack">Jack</Option>
-            <Option value="lucy">Lucy</Option>
-          </Select>
+            <div className="control">
+              Select Pokemon Type:&nbsp;
+              <Select defaultValue="" style={{ width: 120 }} onChange={(type) => handleChangeType(type)}>
+                <Option value="">All Type</Option>
+                { pokemonTypeList
+                  ?.map((type, i) =>
+                <Option value={type.url} key={`type-${i}`}>{type.name}</Option>
+                  )
+                }
+              </Select>
+            </div>
             <Divider />
-            <Row gutter={16}>
+            { loading ? <div className="loading"><LoadingOutlined /></div> : ''}
+            <Row gutter={16} >
               { pokemonList
                 ?.map((pokemon, i) =>
                   <Col className="cust-col" span={6} key={`poke-${i}`}>
